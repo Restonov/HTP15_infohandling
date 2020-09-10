429ec36388123ed7a114ee78407d311b335fbb32
Restonov <maxplyushko@gmail.com>
2020-09-10 18:42:45 +0300

    add base_interpreter

---
+++ src/main/java/by/restonov/infohandling/interpreter/BaseInterpreter.java
@@ -0,0 +1,7 @@
+package by.restonov.infohandling.interpreter;
+
+import by.restonov.infohandling.exception.InterpreterException;
+
+public interface BaseInterpreter {
+    String interpret(String text);
+}
