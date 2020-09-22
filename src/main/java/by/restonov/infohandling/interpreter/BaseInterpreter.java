b3d178051c8b9971c832e5418e31a836ba287059
Restonov <maxplyushko@gmail.com>
2020-09-22 22:47:20 +0300

    refactor interpreter

--- src/main/java/by/restonov/infohandling/interpreter/BaseInterpreter.java
+++
@@ -1,7 +0,0 @@
-package by.restonov.infohandling.interpreter;
-
-import by.restonov.infohandling.exception.InterpreterException;
-
-public interface BaseInterpreter {
-    String interpret(String text);
-}
